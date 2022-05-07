6be95ff798aac2dec33a50a876ce90d560b0fe44
donovan <86621108+pintard@users.noreply.github.com>
2022-05-06 20:16:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ const [, , ...args] = process.argv
 if (process.argv.length === 4) fileCompare(args[0], args[1])
 else if (process.argv.length === 3) directoryCompare(args[0])
 else console.error('Error: requires at least one argument.'
-    + '\n\tmain.js[directory name]- directory analysis'
+    + '\n\tmain.js[directory name] - directory analysis'
     + '\n\tmain.js[file path1][file path2] - file comparison')
